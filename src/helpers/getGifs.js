50455e24c19be27740ab007c10e79efa01fc49f9
Juanje Urbán <juanje.urban@gmail.com>
2022-06-27 13:50:21 +0200

    update

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,16 +1,14 @@
 export const getGifs = async (category) => {
-    const giphyApiUrl = `https://api.giphy.com/v1/gifs/search?api_key=4ewpJM0R4TExtyLHh53F5XHsr6a7fAKF&q=${category}&limit=20`;
-    const resp = await (fetch(giphyApiUrl));
-  
-    const { data } = await resp.json();
-  
-    const gifs = data.map(img => ({
-      id: img.id,
-      title: img.title,
-      url: img.images.downsized_medium.url
-    }));
+  const giphyApiUrl = `https://api.giphy.com/v1/gifs/search?api_key=4ewpJM0R4TExtyLHh53F5XHsr6a7fAKF&q=${category}&limit=20`;
+  const resp = await fetch(giphyApiUrl);
 
-    return gifs;
-  
-    console.log(gifs);
-  }
+  const { data } = await resp.json();
+
+  const gifs = data.map((img) => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url,
+  }));
+
+  return gifs;
+};
